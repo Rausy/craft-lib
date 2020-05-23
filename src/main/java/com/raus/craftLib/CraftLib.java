6fe0739fe8d35d9a62b8b163c176ca1af45db22d
Raus <rausyrasu@gmail.com>
2020-05-22 21:30:30 -0400

    Removed beta code

--- src/main/java/com/raus/craftLib/CraftLib.java
+++ src/main/java/com/raus/craftLib/CraftLib.java
@@ -9,13 +9,8 @@ import java.util.Set;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.ShapelessRecipe;
-import org.bukkit.inventory.meta.EnchantmentStorageMeta;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.google.common.collect.ImmutableList;
@@ -51,95 +46,6 @@ public class CraftLib extends JavaPlugin
 	{
 		// Listeners
 		getServer().getPluginManager().registerEvents(new CraftingListener(), this);
-
-
-		NamespacedKey demeraldKey = new NamespacedKey(this, "demerald");
-		NamespacedKey demeraldSwordKey = new NamespacedKey(this, "demerald_sword");
-		NamespacedKey enchantedBookKey = new NamespacedKey(this, "enchanted_book");
-		NamespacedKey mergeEnchantKey = new NamespacedKey(this, "merge_enchant");
-
-		// Demerald item
-		ItemStack item1 = new ItemStack(Material.EMERALD);
-		ItemMeta meta1 = item1.getItemMeta();
-		meta1.setDisplayName("Demerald");
-		meta1.getPersistentDataContainer().set(demeraldKey, PersistentDataType.BYTE, (byte) 0);
-		item1.setItemMeta(meta1);
-
-		// Demerald recipe
-		ShapelessRecipe recipe1 = new ShapelessRecipe(demeraldKey, item1);
-		recipe1.addIngredient(Material.DIAMOND);
-		recipe1.addIngredient(Material.EMERALD);
-		Bukkit.addRecipe(recipe1);
-
-		addKey(demeraldKey);
-
-
-		// Demerald sword
-		ItemStack item2 = new ItemStack(Material.DIAMOND_SWORD);
-		ItemMeta meta2 = item2.getItemMeta();
-		meta2.setDisplayName("Demerald Sword");
-		meta2.getPersistentDataContainer().set(demeraldSwordKey, PersistentDataType.BYTE, (byte) 0);
-		item2.setItemMeta(meta2);
-
-		// Demerald sword recipe
-		ExactShapedRecipe recipe2 = new ExactShapedRecipe(demeraldSwordKey, item2);
-		recipe2.setIngredient('e', Material.EMERALD, demeraldKey);
-		recipe2.setIngredient('s', Material.STICK);
-		recipe2.setShape("e", "e", "s");
-		addRecipe(recipe2);
-
-
-		// Enchanted book recipe
-		ExactShapelessRecipe recipe3 = new ExactShapelessRecipe(enchantedBookKey, new ItemStack(Material.ENCHANTED_BOOK));
-		recipe3.addIngredient(Material.EXPERIENCE_BOTTLE);
-		recipe3.addIngredient(Material.BOOK);
-		recipe3.setCallback(event ->
-		{
-			ItemStack result = event.getInventory().getResult();
-
-			EnchantmentStorageMeta meta = (EnchantmentStorageMeta) result.getItemMeta();
-			if (Math.random() > 0.5)
-			{
-				meta.addStoredEnchant(Enchantment.DURABILITY, 1, true);
-			}
-			else
-			{
-				meta.addStoredEnchant(Enchantment.DAMAGE_ALL, 1, true);
-			}
-			result.setItemMeta(meta);
-
-			event.getInventory().setResult(result);
-		});
-		addRecipe(recipe3);
-
-		// Merge enchantments
-		ExactShapelessRecipe recipe4 = new ExactShapelessRecipe(mergeEnchantKey, item2);
-		recipe4.addIngredient(Material.DIAMOND_SWORD, demeraldSwordKey);
-		recipe4.addIngredient(Material.ENCHANTED_BOOK);
-		recipe4.setCallback(event ->
-		{
-			ItemStack result = event.getInventory().getResult();
-			ItemMeta meta = result.getItemMeta();
-
-			// Iterate through crafting slots
-			for (ItemStack slot : event.getInventory().getMatrix())
-			{
-				// Find enchantment book
-				if (slot != null && slot.getType() == Material.ENCHANTED_BOOK)
-				{
-					// Transfer enchantments
-					EnchantmentStorageMeta enchMeta = (EnchantmentStorageMeta) slot.getItemMeta();
-					for (Map.Entry<Enchantment, Integer> ench : enchMeta.getStoredEnchants().entrySet())
-					{
-						meta.addEnchant(ench.getKey(), ench.getValue(), false);
-					}
-				}
-			}
-			result.setItemMeta(meta);
-
-			event.getInventory().setResult(result);
-		});
-		addRecipe(recipe4);
 	}
 
 	@Override
